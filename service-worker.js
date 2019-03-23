fdd4119bdea7895a7f51840288058a8242abbdcc
James McDougall <jamesmcdougall@jamess-air-2.dynamic.ucsd.edu>
2019-03-23 14:25:38 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/JamesMcDougallJr/website.git/precache-manifest.87db3f2fc9cf085797a1c614330d955a.js"
+  "/website/precache-manifest.6649045f8c93980cd60d3c107c983df0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/JamesMcDougallJr/website.git/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
